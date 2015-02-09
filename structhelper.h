b61de9d10c794f01bdc97c1969bf08862ee1c31f
Arthur Lockman <hello@rthr.me>
2015-02-08 19:48:58 -0500

    Added documentation to student struct.

--- structhelper.h
+++ structhelper.h
@@ -2,10 +2,10 @@
 #define STRUCTHELPER_H
 
 typedef struct {
-    char* name;
-    int grade;
-    float gpa;
-} Student;
+    char* name; ///< The student's name.
+    int grade; ///< The student's grade.
+    float gpa; ///< The student's current GPA.
+} Student; ///< A struct for holding information about a student.
 
 Student* make_student(char* name, int grade, float gpa);
 void print_student(Student* s);
